efe7231377b6527caa83706397c1df9c8c656c90
suman sah <shahsuman438@gmail.com>
2022-03-28 17:06:25 +0545

    router link to login and register

---
+++ src/pages/register/Register.jsx
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import registerImg from '../../static/loginImg/register2.svg'
+
+import './register.scss'
+
+export class Register extends Component {
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+        
+      }
+    }
+  render() {
+    return (
+        <div className='register'>
+        <div className="header">
+          Register
+        </div>
+        <div className="content">
+          <div className="logo">
+            <img src={registerImg} alt="" />
+          </div>
+        <div className="form">
+        <div className="form-group">
+            <label htmlFor="name">Name</label>
+            <input type="text" name='name' placeholder='Name' />
+          </div>
+          <div className="form-group">
+            <label htmlFor="email">Email</label>
+            <input type="email" name='email' placeholder='Email' />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input type="password" name='password' placeholder='Password' />
+          </div>
+          <div className="form-group">
+            <label htmlFor="Repassword">Password</label>
+            <input type="password" name='Repassword' placeholder='Re-type Password' />
+          </div>
+        </div>
+        <div className="footer">
+          <button type='button' className="btn">Register</button>
+        </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Register
