7d09c1aec36043a9ceedb85fbcad12a7f944bf3e
suman sah <shahsuman438@gmail.com>
2022-03-28 22:54:37 +0545

    single/info div ui

--- src/pages/single/Single.jsx
+++ src/pages/single/Single.jsx
@@ -7,49 +7,49 @@ import Table from '../../components/table/Table'
 export const Single = () => {
   return (
     <div className='single'>
-         <SideBar/>
-         <div className="singleContainer">
-           <NavBar/>
-            <div className="top">
-              <div className="left">
-                <div className="editButton">Edit</div>
-                <h1 className="title">Information</h1>
-                <div className="item"> 
-                  <img src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cmFuZG9tJTIwcGVvcGxlfGVufDB8fDB8fA%3D%3D&w=1000&q=80" 
-                  alt="image" className="itemImg" />
+      <SideBar />
+      <div className="singleContainer">
+        <NavBar />
+        <div className="top">
+          <div className="left">
+            <div className="editButton">Edit</div>
+            <h1 className="title">Information</h1>
+            <div className="item">
+              <img src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cmFuZG9tJTIwcGVvcGxlfGVufDB8fDB8fA%3D%3D&w=1000&q=80"
+                alt="image" className="itemImg" />
 
-                  <div className="details">
-                      <h1 className="itemTitle">
-                        jane Doe
-                      </h1>
-                      <div className="detailItem">
-                        <span className='itemKey'>Email:</span>
-                        <span className="itemValue">joe@gmail.com</span>
-                      </div>
-                      <div className="detailItem">
-                        <span className='itemKey'>phone:</span>
-                        <span className="itemValue">+977-9815213183</span>
-                      </div>
-                      <div className="detailItem">
-                        <span className='itemKey'>Address:</span>
-                        <span className="itemValue">Shreepur 14 Birgunj</span>
-                      </div>
-                      <div className="detailItem">
-                        <span className='itemKey'>Country:</span>
-                        <span className="itemValue">Nepal</span>
-                      </div>
-                  </div>
+              <div className="details">
+                <h1 className="itemTitle">
+                  jane Doe
+                </h1>
+                <div className="detailItem">
+                  <span className='itemKey'>Email:</span>
+                  <span className="itemValue">joe@gmail.com</span>
+                </div>
+                <div className="detailItem">
+                  <span className='itemKey'>phone:</span>
+                  <span className="itemValue">+977-9815213183</span>
+                </div>
+                <div className="detailItem">
+                  <span className='itemKey'>Address:</span>
+                  <span className="itemValue">Shreepur 14 Birgunj</span>
+                </div>
+                <div className="detailItem">
+                  <span className='itemKey'>Country:</span>
+                  <span className="itemValue">Nepal</span>
                 </div>
               </div>
-              <div className="right">
-                <Chart></Chart>
-                </div> 
             </div>
-            <div className="bottom">
-            <h1 className="title">Favourite Contacts</h1>
-              <Table></Table>
-            </div>
-         </div>
+          </div>
+          <div className="right">
+            <Chart></Chart>
+          </div>
+        </div>
+        <div className="bottom">
+          <h1 className="title">Favourite Contacts</h1>
+          <Table></Table>
+        </div>
+      </div>
     </div>
   )
 }
