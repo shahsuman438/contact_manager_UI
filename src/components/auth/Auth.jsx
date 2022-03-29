7bbf69960d588c750f6c93be9a6c567a89b75a02
suman sah <shahsuman438@gmail.com>
2022-03-29 16:25:56 +0545

    auth component wraped with login and register

---
+++ src/components/auth/Auth.jsx
@@ -0,0 +1,23 @@
+
+import React, { useState } from 'react'
+import Login from '../../pages/login/Login'
+import Register from '../../pages/register/Register'
+import './auth.scss'
+
+
+function Auth() {
+    const[islogin,setIsLogIn]=useState(true)
+  return (
+    <div className="auth">
+        <div className="container">
+            <div className="nav">
+                <button className='loginbtn' onClick={()=>setIsLogIn(true)}>Login</button>
+                <button className='registerbtn' onClick={()=>setIsLogIn(false)}>Register</button>
+            </div>
+           {islogin?<Login/>:<Register/>}    
+        </div>
+    </div>
+  )
+}
+
+export default Auth
