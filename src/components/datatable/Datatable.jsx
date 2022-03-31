d3c6cc3cf2bab055dac717d4fd5ac9221b12f1b6
suman sah <shahsuman438@gmail.com>
2022-03-31 09:57:24 +0545

    data table add, edit , delete button style

--- src/components/datatable/Datatable.jsx
+++ src/components/datatable/Datatable.jsx
@@ -1,60 +1,65 @@
-import React, { useState } from 'react'
+import React from 'react'
 import './datatable.scss'
-
-import { DataGrid } from '@mui/x-data-grid';
-
-import { userColumns,userRows } from '../../data/DataSource';
 import { Link } from 'react-router-dom';
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
+import AddIcon from '@mui/icons-material/Add';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
 
-
-
-
-
-
-
-const Datatable = () => {
-  const [data,setData]=useState(userRows)
-
-  const deleteHandler=(id)=>{
-      setData(data.filter(item=>item.id!==id))
-  }
-
-
-
-  const actionColumn=[
-    {
-      field:"action", headerName:"ACTION", width:300,renderCell:(params)=>{
-        return (
-          <div className='cellAction'>
-            <Link to="/contacts/new" style={{textDecoration:"none"}}>
-                <div className="viewButton">View</div>
-            </Link>
-              <div className="deleteButton" onClick={()=>deleteHandler(params.row.id)}>Delete</div>
-          </div>
-        )
-      }
-    }
-  ]
+const Datatable = (props) => {
   return (
     <div className='datatable' >
-      <div className="datatableTitle">
-        Add New Contact 
+
+    <div className="datatableTitle">
+        Contact List
         <Link to="/contacts/new" className='link' style={{textDecoration:"none"}}>
-          Add
+          <AddIcon className='add'/>
         </Link>
-      </div>
-       <DataGrid 
-       className='dataGrid'
-        rows={data}
-        columns={userColumns.concat(actionColumn)}
-        pageSize={10}
-        rowsPerPageOptions={[10]}
-        checkboxSelection
-      />
+    </div>
+    <TableContainer component={Paper} className="table">
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell align="left" className='tablecell'>photo</TableCell>
+            <TableCell className='tablecell'>Name</TableCell>
+            <TableCell align="center" className='tablecell'>Number</TableCell>
+            <TableCell align="center" className='tablecell'>Email</TableCell>
+            <TableCell align="center" className='tablecell'>Address</TableCell>
+            <TableCell align="center" className='tablecell'>Action</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {props.value.map((row) => (
+            <TableRow key={row._id}>
+              <TableCell align="right" className='tablecell'>
+                  <div className="cellWrapper">
+                      <img src={row.photo?`http://localhost:4000/${row.photo}`:"https://fupping.com/wp-content/uploads/2018/06/Personal.png"} alt="" className="image" />
+                  </div>
+              </TableCell>
+              <TableCell component="th" scope="row" className='tablecell'>{row.name}</TableCell>
+              <TableCell align="center" className='tablecell'>{row.number}</TableCell>
+              <TableCell align="center" className='tablecell'>{row.email}</TableCell>
+              <TableCell align="center" className='tablecell'>{row.address}</TableCell>
+              <TableCell align="center" className='tablecell action'>
+                <button className='editButton'><EditIcon/></button>
+                <button className='deleteButton'><DeleteIcon/></button>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
     </div>
   )
 }
+
 
 export default Datatable
 
--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -1,16 +1,35 @@
-import React from 'react'
+import React ,{useEffect,useState} from 'react'
 import Sidebar from '../../components/sidebar/SideBar'
 import Navbar from '../../components/navbar/NavBar'
 import './list.scss'
 import Datatable from '../../components/datatable/Datatable'
+import axios from 'axios'
 
 export const List = () => {
+  const [data,setData]=useState([])
+  const authKey=localStorage.getItem('authorization')
+  const authaxios=axios.create({
+    baseURL:"http://localhost:4000/",
+    headers:{
+        Authorization:`Bearer ${authKey}`
+    }
+  })
+  useEffect(() => {
+    authaxios.get('contact')
+    .then( result=>{
+       setData(result.data)
+    })
+    .catch(error=>{
+      console.log(error.response.data)
+   })
+  
+   })
   return (
     <div className='list'>
       <Sidebar/>
       <div className="listContainer">
           <Navbar/>
-          <Datatable/>
+          <Datatable value={data}/>
       </div>
     </div>
   )
