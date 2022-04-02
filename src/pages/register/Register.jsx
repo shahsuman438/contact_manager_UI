30a6dcbc7c63f5c4bc0c33fb56f704ec73c9b5f0
suman sah <shahsuman438@gmail.com>
2022-04-02 15:43:17 +0545

    register message styleing change

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,70 +1,70 @@
-import React, { Component ,useState} from 'react';
+import React, { Component, useState } from 'react';
 import registerImg from '../../static/loginImg/register2.svg'
 import { useNavigate } from 'react-router-dom';
 
 import './register.scss'
 import axios from 'axios'
 
-const initialState={
-  name:'',
-  email:'',
-  password:''
+const initialState = {
+  name: '',
+  email: '',
+  password: ''
 }
 
 function Register() {
-  const [register,setRegister]=useState(initialState)
-  const [msg,setMsg]=useState('')
-  const [rePass,setRePass]=useState('')
-  const navigate=useNavigate()
+  const [register, setRegister] = useState(initialState)
+  const [msg, setMsg] = useState('')
+  const [rePass, setRePass] = useState('')
+  const navigate = useNavigate()
 
-  const submitHandler=(event)=>{
-        if(register.password!==rePass){
-          setMsg("Password not matched")
-        }else{
-          setMsg("")
-          axios.post('http://localhost:4000/auth/register',register)
-          .then(res=>{
-            localStorage.setItem("authorization",res.data.token)
-            navigate("/")
-          }).catch(error=>{
-            setMsg(error.response.data.msg)
-          })
-        }
-        event.preventDefault()
- }
+  const submitHandler = (event) => {
+    if (register.password !== rePass) {
+      setMsg("Password not matched")
+    } else {
+      setMsg("")
+      axios.post('http://localhost:4000/auth/register', register)
+        .then(res => {
+          localStorage.setItem("authorization", res.data.token)
+          navigate("/")
+        }).catch(error => {
+          setMsg(error.response.data.msg)
+        })
+    }
+    event.preventDefault()
+  }
   return (
     <div className='register'>
-        <div className="header">
-          Register
+      <div className="header">
+        Register
+      </div>
+      <form className="content" onSubmit={submitHandler}>
+        <div className="logo">
+          <img src={registerImg} alt="" />
         </div>
-        <form className="content" onSubmit={submitHandler}>
-          <div className="logo">
-            <img src={registerImg} alt="" />
-          </div>
         <div className="form">
-        <div className="form-group">
+          <div className="form-group">
             <label htmlFor="name">Name</label>
-            <input type="text" name='name' placeholder='Name' onChange={(e)=>setRegister({...register,name:e.target.value})} required />
+            <input type="text" name='name' placeholder='Name' onChange={(e) => setRegister({ ...register, name: e.target.value })} required />
           </div>
           <div className="form-group">
             <label htmlFor="email">Email</label>
-            <input type="email" name='email' placeholder='Email' onChange={(e)=>setRegister({...register,email:e.target.value})} required />
+            <input type="email" name='email' placeholder='Email' onChange={(e) => setRegister({ ...register, email: e.target.value })} required />
           </div>
           <div className="form-group">
             <label htmlFor="password">Password</label>
-            <input type="password" name='password' placeholder='Password' onChange={(e)=>setRegister({...register,password:e.target.value})} required />
+            <input type="password" name='password' placeholder='Password' onChange={(e) => setRegister({ ...register, password: e.target.value })} required />
           </div>
           <div className="form-group">
-            <label htmlFor="Repassword">Password</label>
-            <input type="password" name='Repassword' placeholder='Re-type Password' onChange={(e)=>setRePass(e.target.value)} required />
+            <label htmlFor="Repassword">Confirm Password</label>
+            <input type="password" name='Repassword' placeholder='Re-type Password' onChange={(e) => setRePass(e.target.value)} required />
           </div>
         </div>
         <p className='errorMsg'>{msg}</p>
         <div className="footer">
           <button type='submit' className="btn">Register</button>
         </div>
-        </form>
-      </div>
+      </form>
+    </div>
   )
 }
 
