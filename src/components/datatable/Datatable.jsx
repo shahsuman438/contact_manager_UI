cdf11a27bcdcc34f125f52d4e800a46baba07510
suman sah <shahsuman438@gmail.com>
2022-04-06 09:51:26 +0545

    number styling done

--- src/components/datatable/Datatable.jsx
+++ src/components/datatable/Datatable.jsx
@@ -1,4 +1,4 @@
-import React, { useState,useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import './datatable.scss'
 import { Link } from 'react-router-dom';
 import DeleteIcon from '@mui/icons-material/Delete';
@@ -15,72 +15,81 @@ import authAxios from '../../interceptors/axios';
 import { toast } from 'react-toastify';
 
 const Datatable = () => {
-  const [query,setQuery]=useState('')
-  const [data,setData]=useState([])
-  const deleteHandler=(e)=>{
+  const [query, setQuery] = useState('')
+  const [data, setData] = useState([])
+  const deleteHandler = (e) => {
     authAxios.delete(`contact/${e}`)
-    .then( result=>{
-       toast.success(result.data.msg)
-    })
-    .catch(error=>{
-      toast.error(`${error.response.status} ${error.response.statusText}`)
-   })
+      .then(result => {
+        toast.success(result.data.msg)
+      })
+      .catch(error => {
+        toast.error(`${error.response.status} ${error.response.statusText}`)
+      })
   }
   useEffect(() => {
     authAxios.get('contact')
-    .then( result=>{
-       setData(result.data.slice(0).reverse())
-    })
-    .catch(error=>{
-      console.log(error.response.data)
-   })
-   },[])
+      .then(result => {
+        setData(result.data.slice(0).reverse())
+      })
+      .catch(error => {
+        console.log(error.response.data)
+      })
+  }, [])
 
   return (
     <div className='datatable' >
-    <div className="datatableTitle">
+      <div className="datatableTitle">
         Contact List
-        <input className='search' type="text" placeholder='Search....' onChange={(e)=>setQuery(e.target.value)} />
-        <Link to="/contacts/new" className='link' style={{textDecoration:"none"}} value={"value from data table"}>
-          <AddIcon className='add'/>
+        <input className='search' type="text" placeholder='Search....' onChange={(e) => setQuery(e.target.value)} />
+        <Link to="/contacts/new" className='link' style={{ textDecoration: "none" }} value={"value from data table"}>
+          <AddIcon className='add' />
         </Link>
-    </div>
-    <TableContainer component={Paper} className="table">
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell align="left" className='tablecell'>photo</TableCell>
-            <TableCell className='tablecell'>Name</TableCell>
-            {/* <TableCell align="center" className='tablecell'>Number</TableCell> */}
-            <TableCell align="center" className='tablecell'>Email</TableCell>
-            <TableCell align="center" className='tablecell'>Address</TableCell>
-            <TableCell align="center" className='tablecell'>Action</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {data.filter(item=>item.name.toLowerCase().includes(query)).map((row) => (
-            <TableRow key={row._id}>
-              <TableCell align="right" className='tablecell'>
-                  <div className="cellWrapper">
-                      <img src={row.photo?`http://localhost:4000/${row.photo}`:"https://fupping.com/wp-content/uploads/2018/06/Personal.png"} alt="" className="image" />
-                      {row.fav?<div className="fav"></div>:null}
-                  </div>
-              </TableCell>
-              <TableCell component="th" scope="row" className='tablecell'>{row.name}</TableCell>
-              {/* <TableCell align="center" className='tablecell'>+977-{row.number}</TableCell> */}
-              <TableCell align="center" className='tablecell'>{row.email}</TableCell>
-              <TableCell align="center" className='tablecell'>{row.address}</TableCell>
-              <TableCell align="center" className='tablecell action'>
-              <Link to={{pathname:`edit/${row._id}`}} className='link' style={{textDecoration:"none"}} value={"value from data table"}>
-                    <button className='editButton' ><EditIcon/></button>
-              </Link>
-                <button className='deleteButton' onClick={()=>deleteHandler(row._id)} ><DeleteIcon/></button>
-              </TableCell>
+      </div>
+      <TableContainer component={Paper} className="table">
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell align="left" className='tablecell'>photo</TableCell>
+              <TableCell className='tablecell'>Name</TableCell>
+              <TableCell align="center" className='tablecell'>Number</TableCell>
+              <TableCell align="center" className='tablecell'>Email</TableCell>
+              <TableCell align="center" className='tablecell'>Address</TableCell>
+              <TableCell align="center" className='tablecell'>Action</TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {data.filter(item => item.name.toLowerCase().includes(query)).map((row) => (
+              <TableRow key={row._id}>
+                <TableCell align="right" className='tablecell'>
+                  <div className="cellWrapper">
+                    <img src={row.photo ? `http://localhost:4000/${row.photo}` : "https://fupping.com/wp-content/uploads/2018/06/Personal.png"} alt="" className="image" />
+                    {row.fav ? <div className="fav"></div> : null}
+                  </div>
+                </TableCell>
+                <TableCell component="th" scope="row" className='tablecell'>{row.name}</TableCell>
+                <TableCell align="center" className='tablecell numbers'>
+                  {
+                    row.number.slice(-2).map((num) =>
+                      <div className="number ">
+                        <p>{num.name}:</p>
+                        <p>+977-{num.number}</p>
+                      </div>
+                    )
+                  }
+                </TableCell>
+                <TableCell align="center" className='tablecell'>{row.email}</TableCell>
+                <TableCell align="center" className='tablecell'>{row.address}</TableCell>
+                <TableCell align="center" className='tablecell action'>
+                  <Link to={{ pathname: `edit/${row._id}` }} className='link' style={{ textDecoration: "none" }} value={"value from data table"}>
+                    <button className='editButton' ><EditIcon /></button>
+                  </Link>
+                  <button className='deleteButton' onClick={() => deleteHandler(row._id)} ><DeleteIcon /></button>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </div>
   )
 }
--- src/components/table/Table.jsx
+++ src/components/table/Table.jsx
@@ -16,7 +16,7 @@ function Tables(props) {
           <TableRow>
             <TableCell align="left" className='tablecell'>photo</TableCell>
             <TableCell className='tablecell'>Name</TableCell>
-            {/* <TableCell align="center" className='tablecell'>Number</TableCell> */}
+            <TableCell align="center" className='tablecell'>Number</TableCell>
             <TableCell align="center" className='tablecell'>Email</TableCell>
             <TableCell align="center" className='tablecell'>Address</TableCell>
           </TableRow>
@@ -25,12 +25,19 @@ function Tables(props) {
           {props.value.map((row) => (
             <TableRow key={row._id}>
               <TableCell align="right" className='tablecell'>
-                  <div className="cellWrapper">
-                      <img src={row.photo?`http://localhost:4000/${row.photo}`:"https://fupping.com/wp-content/uploads/2018/06/Personal.png"} alt="" className="image" />
-                  </div>
+                <div className="cellWrapper">
+                  <img src={row.photo ? `http://localhost:4000/${row.photo}` : "https://fupping.com/wp-content/uploads/2018/06/Personal.png"} alt="" className="image" />
+                </div>
               </TableCell>
               <TableCell component="th" scope="row" className='tablecell'>{row.name}</TableCell>
-              {/* <TableCell align="center" className='tablecell'>{row.number}</TableCell> */}
+              <TableCell align="center" className='tablecell numbers'>{
+                row.number.slice(-2).map((num) =>
+                  <div className="number ">
+                    <p>{num.name}:</p>
+                    <p>+977-{num.number}</p>
+                  </div>
+                )
+              }</TableCell>
               <TableCell align="center" className='tablecell'>{row.email}</TableCell>
               <TableCell align="center" className='tablecell'>{row.address}</TableCell>
             </TableRow>
